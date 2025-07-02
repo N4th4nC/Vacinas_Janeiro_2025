aee04236b9ad443807f3beb3e517a9b8fade7bcc
N4th4nC <nathan.caldeir4@gmail.com>
2025-07-02 03:55:14 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pa
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+
+
+
+
+# =======================
+# LAYOUT DA PÁGINA
+# =======================
+st.title("Painel de Vacinação no Brasil")
+
